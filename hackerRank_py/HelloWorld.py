d605455466e3379dfe8accdc1a79528238d1f35c
Juliana Stopassoli Barazal <julianabarazal545@gmail.com>
2024-04-23 19:56:28 -0300

    primeiro commit

---
+++ hackerRank_py/HelloWorld.py
@@ -0,0 +1,3 @@
+#print hello world
+str = "Hello, World!"
+print(str)
---
+++ hackerRank_py/If-Else.py
@@ -0,0 +1,11 @@
+#First if-else in python
+if n%2 != 0:
+    print("Weird")
+
+else:
+    if n >=2 and n<=5 or n>20:
+        print("Not Weird")
+        
+    elif n>=6 and n<=20:
+        print("Weird")
+        
